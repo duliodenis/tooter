7ef5ead876501cf494e0175b0f8499cf022b71a5
Dulio Denis <dulio.denis@yahoo.com>
2016-02-13 14:24:05 -0500

    Added Custom UITableViewCell for the TableView

---
+++ Tooter/Tooter/Views/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  Tooter
+//
+//  Created by Dulio Denis on 2/13/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
---
+++ Tooter/Tooter/Views/VideoCell.m
@@ -0,0 +1,36 @@
+//
+//  VideoCell.m
+//  Tooter
+//
+//  Created by Dulio Denis on 2/13/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+#import "VideoCell.h"
+
+@interface VideoCell()
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnail;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (weak, nonatomic) IBOutlet UIView *cellView;
+@end
+
+
+@implementation VideoCell
+
+- (void)awakeFromNib {
+    // Styling of the cell
+    self.layer.cornerRadius = 3.0;
+    self.layer.shadowColor = [UIColor colorWithRed:157.0/255.0 green:157.0/255.0 blue:157.0/255.0 alpha:0.8].CGColor;
+    self.layer.shadowOpacity = 0.8;
+    self.layer.shadowRadius = 5.0;
+    self.layer.shadowOffset = CGSizeMake(0.0, 2.0);
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
